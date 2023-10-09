346f1ae5486b50e10de010d5144877e05399abe4
Shaw <shawwian@gmail.com>
2023-10-09 01:39:46 -0400

    change addr flag to port

--- websocket_server/main.go
+++ websocket_server/main.go
@@ -10,11 +10,12 @@ import (
 var debugPrint bool
 
 func main() {
-	var addr string
-	flag.StringVar(&addr, "addr", ":3333", "http service address")
+	var port string
+	flag.StringVar(&port, "port", "3333", "port number for server to listen on")
 	flag.BoolVar(&debugPrint, "debug", false, "enable debug printing")
 
 	flag.Parse()
+	addr := ":" + port
 	hub := newHub()
 	go hub.run()
 	http.HandleFunc("/proxchat", func(w http.ResponseWriter, r *http.Request) {
@@ -24,7 +25,7 @@ func main() {
 		Addr: addr,
 		ReadHeaderTimeout: 3 * time.Second,
 	}
-	log.Printf("Listening at %s...\n", addr)
+	log.Printf("Listening on port %s...\n", port)
 	err := server.ListenAndServe()
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
